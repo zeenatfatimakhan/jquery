928ad5d83caa9ecc7f9a6f15c2f7a331a3a86560
Zeenatfatimakhan <ijmam27@gmail.com>
2024-03-15 12:59:39 +0500

    update

---
+++ README.md
@@ -0,0 +1,2 @@
+# porfolio code
+this sample repo!
---
+++ jquer.js
@@ -0,0 +1,20 @@
+function sub(i,j){
+let sum=i-j;
+return sum;
+}
+
+console.log(sub(67,6));
+
+function mul(i,j){
+let mul=i*j;
+return mul;
+}
+
+console.log(mul(67,6));
+
+function modulo(i,j){
+let modulo=i%j;
+return modulo;
+}
+
+console.log(modulo(6,6));
